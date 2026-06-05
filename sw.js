09a5adfff64827e55ce37410cbb1f5af63a84ae3
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-06-05 10:47:14 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'lec-flix-test-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './styles.css',
+  './main.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+     .then(cache => cache.addAll(urlsToCache))
+     .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key!== CACHE_NAME? caches.delete(key) : null))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+     .then(res => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, res.clone());
+          return res;
+        });
+      })
+     .catch(() => caches.match('./index.html'))
+  );
+});
