edec677ed3b9b2d56640ebb644df210c1b9b9930
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-06-05 12:04:10 +0200

    Update loaderjson.js

--- data/loaderjson.js
+++ data/loaderjson.js
@@ -1,1 +1,29 @@
+// /data/loaderjson.js
+export async function loadAllBancs() {
+  const bancsFiles = [
+    'banco_estructura.json',
+    'banco_generes.json',
+    'banco_personatges.json',
+    'banco_escenarios.json',
+    'banco_emocions.json',
+    'banco_ecenes.json',
+    'banco_lectura.json',
+    'determinants.json',
+    'banco_vocabulari.json',
+    'banco_preguntes.json',
+    'banco_ubicacions.json',
+    'banco_temes.json'
+  ];
+
+  const bancs = {};
+  const promises = bancsFiles.map(async (file) => {
+    const res = await fetch(`./data/${file}`);
+    if (!res.ok) throw new Error(`No s'ha pogut carregar ${file}`);
+    const key = file.replace('.json', '');
+    bancs[key] = await res.json();
+  });
+
+  await Promise.all(promises);
+  return bancs;
+}
 
