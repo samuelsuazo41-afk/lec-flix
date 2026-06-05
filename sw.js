5b51ab59f1caa64a70ea7070bafa6ecbfe14464d
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-06-05 15:39:20 +0200

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,54 @@
-const CACHE_NAME = 'lec-flix-test-v14';
+const CACHE_NAME = 'lec-flix-test-v15';
+
 const urlsToCache = [
   './',
   './index.html',
   './styles.css',
   './main.js',
-  './manifest.json'
+  './manifest.json',
+  './sw.js',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-512-maskable.png',
+  './core/generadorlilibre.js',
+  './data/loaderjson.js',
+  './data/banco_ecenes.json',
+  './data/banco_emocions.json',
+  './data/banco_escenarios.json',
+  './data/banco_estructura.json',
+  './data/banco_generes.json',
+  './data/banco_lectura.json',
+  './data/banco_olors.json',
+  './data/banco_personatge.json',
+  './data/banco_sons.json',
+  './data/banco_ubicacion.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-     .then(cache => cache.addAll(urlsToCache))
-     .then(() => self.skipWaiting())
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(key => key!== CACHE_NAME? caches.delete(key) : null))
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
     ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request)
-     .then(res => {
+    caches.match(event.request)
+      .then(res => res || fetch(event.request).then(response => {
         return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, res.clone());
-          return res;
+          cache.put(event.request, response.clone());
+          return response;
         });
-      })
-     .catch(() => caches.match('./index.html'))
+      }))
+      .catch(() => caches.match('./index.html'))
   );
 });
