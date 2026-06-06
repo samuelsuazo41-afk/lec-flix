30ce4464dcd650c980d4cc28b1a24e2e65d349cd
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-06-06 15:09:08 +0200

    Actualizar loaderjson.js

--- data/loaderjson.js
+++ data/loaderjson.js
@@ -1,4 +1,7 @@
 export async function loadAllBancs() {
+  // Ruta base robusta per GitHub Pages
+  const baseURL = new URL('../', import.meta.url).href;
+  
   const bancsFiles = [
     'banco_generes.json',
     'banco_estructura.json',
@@ -15,7 +18,7 @@ export async function loadAllBancs() {
 
   await Promise.all(bancsFiles.map(async (file) => {
     try {
-      const res = await fetch(`./data/${file}`);
+      const res = await fetch(baseURL + 'data/' + file);
       if (!res.ok) throw new Error(`No s'ha pogut carregar ${file}`);
       const data = await res.json();
       const key = file.replace('.json', '');
@@ -25,6 +28,6 @@ export async function loadAllBancs() {
     }
   }));
 
-  console.log('Bancs carregats:', Object.keys(bancs)); // DEBUG
+  console.log('Bancs carregats:', Object.keys(bancs));
   return bancs;
 }
