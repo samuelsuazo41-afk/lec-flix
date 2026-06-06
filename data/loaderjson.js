00c47f4c74f793d9f9942cb09d05d8c802336d45
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-06-06 11:11:27 +0200

    Actualizar loaderjson.js

--- data/loaderjson.js
+++ data/loaderjson.js
@@ -1,25 +1,30 @@
-// /data/loaderjson.js
 export async function loadAllBancs() {
   const bancsFiles = [
-    'banco_estructura.json',
     'banco_generes.json',
-    'banco_personatge.json',
-    'banco_escenarios.json',
-    'banco_emocions.json',
+    'banco_estructura.json',
+    'banco_personatge.json', // SENSE S
+    'banco_escenarios.json', // SENSE S
     'banco_lectura.json',
+    'banco_emocions.json',
     'banco_olors.json',
     'banco_sons.json',
-    'banco_ubicacion.json'
+    'banco_ubicacion.json' // SENSE S
   ];
 
   const bancs = {};
-  const promises = bancsFiles.map(async (file) => {
-    const res = await fetch(`./data/${file}`);
-    if (!res.ok) throw new Error(`No s'ha pogut carregar ${file}`);
-    const key = file.replace('.json', '');
-    bancs[key] = await res.json();
-  });
 
-  await Promise.all(promises);
+  await Promise.all(bancsFiles.map(async (file) => {
+    try {
+      const res = await fetch(`./data/${file}`);
+      if (!res.ok) throw new Error(`No s'ha pogut carregar ${file}`);
+      const data = await res.json();
+      const key = file.replace('.json', '');
+      bancs[key] = data;
+    } catch (err) {
+      console.error(`Error carregant ${file}:`, err);
+    }
+  }));
+
+  console.log('Bancs carregats:', Object.keys(bancs)); // DEBUG
   return bancs;
 }
