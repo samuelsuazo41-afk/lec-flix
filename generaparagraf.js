6bfdcd086578e651d0be2d6241c908115138b523
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-06-08 20:06:32 +0200

    Actualizar generaparagraf.js

--- generaparagraf.js
+++ generaparagraf.js
@@ -61,8 +61,8 @@ function netejaEspais(text) {
 
 function forçaPassat(text) {
   return text.replace(/\bMira\b/g, 'Va mirar')
-         .replace(/\bOlía\b/g, 'Feia olor')
-         .replace(/\bSe le congeló\b/g, 'Se li va gelar');
+        .replace(/\bOlía\b/g, 'Feia olor')
+        .replace(/\bSe le congeló\b/g, 'Se li va gelar');
 }
 
 function safeReplace(text, vars) {
@@ -122,7 +122,7 @@ export async function generaParagraf(config, bancs, hist, numCap, numEsc, totalC
   const so = soObj? getTextoBase(soObj) : 'silenci';
   const emocio = emocioObj? getTextoBase(emocioObj) : 'inquietud';
 
-  if (olorObj) hist.usosOlor[olor] = (hist.usosOlor[olor] || 0) + 1;
+  if (olorObj) hist.usosOlor = (hist.usosOlor || 0) + 1;
   if (soObj) hist.usosSo[so] = (hist.usosSo[so] || 0) + 1;
 
   const progress = numCap / totalCaps;
@@ -151,7 +151,7 @@ export async function generaParagraf(config, bancs, hist, numCap, numEsc, totalC
     'giro2': `Res era el que semblava a ${escenari.nom}. ${nom} amb ${emocio} va entendre que havia estat manipulat. L'olor de ${olor} ara sabia a traïció. ${ticActual}.`,
     'crisi': `A ${escenari.nom} tot s'esfondrava. ${nom} amb ${emocio} extrema va veure com l'olor de ${olor} s'esvaïa i el ${so} s'apagava. ${ticActual}.`,
     'climax': beatAnterior === 'crisi'
-   ? `Després de la crisi a ${escenari.nom}, ${nom} va avançar amb ${emocio} pura cap a l'enfrontament final. ${ticActual}. ${olor} i ${so} marcaven el ritme del final.`
+  ? `Després de la crisi a ${escenari.nom}, ${nom} va avançar amb ${emocio} pura cap a l'enfrontament final. ${ticActual}. ${olor} i ${so} marcaven el ritme del final.`
       : `L'enfrontament final a ${escenari.nom}. ${nom} va avançar amb ${emocio} pura mentre ${olor} i ${so} marcaven el ritme del final. ${ticActual}.`,
     'resolucio': `${nom} va quedar sol a ${escenari.nom} després de la tempesta. ${emocio} es transformava en pau mentre l'olor de ${olor} es netejava. ${ticActual}.`,
     'default': `${nom} va continuar a ${escenari.nom} amb ${emocio}, ${olor} i ${so} de fons. ${ticActual}.`
