019f1bbbe6ca5ce98c1b1f4530fa46d63fc682a9
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-06-09 19:02:47 +0200

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js - Service Worker V12.1.2 lec-flix policial
 // Cachea tots els arxius + 19 bancs data per funcionar offline
 
-const CACHE_NAME = 'lec-flix-v12.1.2';
+const CACHE_NAME = 'lec-flix-v12.3.3';
 
 const urlsToCache = [
   './',
